8bbe391bf95e85bc5039808a2e19ad59378e4fbc
Francisco Barriocanal García <uo245503@uniovi.es>
2021-01-27 00:54:01 +0100

    Cuando chocas contra el fuego te quita un corazón
    El fuego duele

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovementScript : MonoBehaviour
 {
@@ -11,11 +12,29 @@ public class MovementScript : MonoBehaviour
     private Rigidbody2D m_Rigidbody2D;
     private GameObject colisionado;
     private Animator anims;
+
+    //Vida del jugador
+    public Image Corazon;
+    public int numCorazones;
+    public RectTransform PosPrimerCorazon;
+    public Canvas MyCanvas;
+    public int OffSet;
+
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
         anims = GetComponent<Animator>();
+
+        Transform PosCorazon = PosPrimerCorazon;
+
+        //Vida del jugador
+        for(int i = 0; i< numCorazones; i++)
+        {
+            Image NewCorazon = Instantiate(Corazon, PosCorazon.position, Quaternion.identity);
+            NewCorazon.transform.parent = MyCanvas.transform;
+            PosCorazon.position = new Vector2(PosCorazon.position.x + OffSet, PosCorazon.position.y);
+        }
     }
 
     // Update is called once per frame
@@ -31,6 +50,12 @@ public class MovementScript : MonoBehaviour
             {
                 aux.Interactuar();
             }
+        }
+        //Si la vida del jugador llega a 0 se muere
+        if (numCorazones <= 0)
+        {
+            Destroy(gameObject);
+            Destroy(Corazon);
         }
     }
 
@@ -58,6 +83,13 @@ public class MovementScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         colisionado = collision.gameObject;
+
+        //Si choca contra un fuego se quema
+        if(colisionado.tag == "Fuego")
+        {
+            Destroy(MyCanvas.transform.GetChild(numCorazones + 1).gameObject);
+            numCorazones -= 1;
+        }
     
     }
     private void OnCollisionExit2D(Collision2D collision)
