f4d9d94aa25718a45044cefece702e9926633e9c
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2021-01-26 21:10:10 +0100

    Añadido el script de al piedra, aun no hace nada

--- Assets/PiedraMovil.cs
+++ Assets/PiedraMovil.cs
@@ -2,39 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour
+public class PiedraMovil : MonoBehaviour
 {
-    public CharacterController2D controller;
+
     public float runSpeed = 40f;
     public float horizontalMove = 0f;
     public float verticalMove = 0f;
     private Rigidbody2D m_Rigidbody2D;
     private GameObject colisionado;
-    private Animator anims;
+   
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        anims = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove=Input.GetAxisRaw("Horizontal");
-        verticalMove=Input.GetAxisRaw("Vertical");
         
-        if (Input.GetKeyDown(KeyCode.E)&&colisionado)
-        {
-            InteractuAR aux = colisionado.GetComponent<InteractuAR>();
-            if (aux)
-            {
-                aux.Interactuar();
-            }
-        }
     }
-
-    private void FixedUpdate()
+     private void FixedUpdate()
     {
         Moverse();
 
@@ -44,13 +32,13 @@ public class MovementScript : MonoBehaviour
     {
         if (horizontalMove >0)
         {
-            anims.SetInteger("moveType", 2);
            
-            anims.SetInteger("direction", 4);
+           
+            
         }
         else
         {
-             anims.SetInteger("moveType", 1);
+           
 
         }
         Vector2 direccion = new Vector2(horizontalMove, verticalMove);
