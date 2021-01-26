eaaeb57b9267c045a29fdbb49eb39f269dbef46f
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2021-01-26 23:36:30 +0100

    Añadido el cofre, falta que haga algo

---
+++ Assets/Cofre.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre : MonoBehaviour
+{
+    private Rigidbody2D m_Rigidbody2D;
+    private GameObject colisionado;
+    private Rigidbody2D aux ;
+    public Transform posicion;
+    private GameObject orbe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         aux = colisionado.GetComponent<Rigidbody2D>();
+        if (aux)
+        {
+           FinNivel();
+        }
+    }
+    private void FinNivel(){
+        Instantiate(orbe, posicion.position, posicion.rotation);
+        Destroy(this.gameObject); 
+    }
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        colisionado = collision.gameObject;
+    
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        colisionado = null;
+    }
+}
